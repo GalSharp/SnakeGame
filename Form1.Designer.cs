6f27da1a855ef354c1d739df605a550446be7f8c
1mp3ra70r <132915375+GalSharp@users.noreply.github.com>
2023-05-28 18:36:46 +0300

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,71 @@
+﻿namespace Snake
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.timer = new System.Windows.Forms.Timer(this.components);
+            this.scoreLB = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // timer
+            // 
+            this.timer.Enabled = true;
+            this.timer.Interval = 250;
+            // 
+            // scoreLB
+            // 
+            this.scoreLB.AutoSize = true;
+            this.scoreLB.Font = new System.Drawing.Font("Showcard Gothic", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.scoreLB.Location = new System.Drawing.Point(810, 20);
+            this.scoreLB.Name = "scoreLB";
+            this.scoreLB.Size = new System.Drawing.Size(117, 30);
+            this.scoreLB.TabIndex = 0;
+            this.scoreLB.Text = "Score : 0";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(984, 811);
+            this.Controls.Add(this.scoreLB);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "SnakeGame";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Timer timer;
+        private System.Windows.Forms.Label scoreLB;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,240 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+
+namespace Snake
+{
+    public partial class Form1 : Form
+    {
+        int directionX;
+        int directionY;
+
+        string tempMove; // переменная для запоминания предыдущей клавиши движения
+
+        Point fruitLocation;
+
+        int score = 0;
+
+        const int unitSize = 40;
+
+        List<PictureBox> snake = new List<PictureBox>();
+
+        PictureBox fruit;
+        PictureBox unit;
+
+        Random random = new Random();
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            // Подписка на ивенты
+            this.KeyDown += GetInput;
+            timer.Tick += Update;
+
+            // Генерация карты
+            MapGenerate();
+
+            // Создание змейки
+            unit = new PictureBox();
+            unit.Size = new Size(unitSize, unitSize);
+            unit.Location = new Point(40, 40);
+            unit.BackColor = Color.Lime;
+
+            Controls.Add(unit);
+
+            snake.Add(unit);
+
+            // Задание направления движения змейки
+            directionX = 1;
+            directionY = 0;
+
+            // Генерация фруктов
+            FruitGenerate();
+        }
+
+        private void StopGameTrigger()
+        {
+            if (snake[0].Location.X > 760 || snake[0].Location.X < 0 || snake[0].Location.Y > 760 || snake[0].Location.Y < 0)
+            {
+                GameOver();
+            }
+
+            if (score > 0)
+            {
+                for (int i = snake.Count - 1; i >= 1; i--)
+                {
+                    if (snake[0].Location == snake[i].Location) GameOver();
+                }
+            }
+        }
+
+        private void MapGenerate()
+        {
+            for (int i = 0; i <= 800; i += 40)
+            {
+                // создание вехрних стен
+                PictureBox topSideWall = new PictureBox();
+
+                topSideWall.BackColor = Color.Black;
+                topSideWall.Size = new Size(800, 1);
+                topSideWall.Location = new Point(topSideWall.Location.X, i);
+
+                Controls.Add(topSideWall);
+
+                // создание боковых стен
+                PictureBox leftSideWall = new PictureBox();
+
+                leftSideWall.BackColor = Color.Black;
+                leftSideWall.Size = new Size(1, 800);
+                leftSideWall.Location = new Point(i, leftSideWall.Location.Y);
+
+                Controls.Add(leftSideWall);
+            }
+        }
+
+        private void FruitGenerate()
+        {
+            fruit = new PictureBox();
+
+            fruit.Size = new Size(unitSize, unitSize);
+            fruit.BackColor = Color.Gold;
+
+            int fruitX = 0;
+            int fruitY = 0;
+
+            bool isSnake = true;
+
+            while (isSnake)
+            {
+                fruitX = random.Next(1, 20);
+                fruitY = random.Next(1, 20);
+
+                foreach (var unit in snake)
+                {
+                    if (unit.Location == new Point(fruitX * unitSize, fruitY * unitSize)) break;
+
+                    isSnake = false;
+                    break;
+                }
+            }
+
+            fruit.Location = new Point(fruitX * unitSize, fruitY * unitSize);
+            fruitLocation = fruit.Location;
+
+            Controls.Add(fruit);
+        }
+
+        private void EatFruit()
+        {
+            if (snake[0].Location == fruitLocation)
+            {
+                score++;
+                scoreLB.Text = $"Score : {score}";
+
+                Controls.Remove(fruit);
+
+                FruitGenerate();
+
+                unit = new PictureBox();
+                unit.Size = new Size(unitSize, unitSize);
+                unit.Location = new Point(snake.Last().Location.X - directionX * unitSize, snake.Last().Location.Y - directionY * unitSize);
+                unit.BackColor = Color.Lime;
+
+                snake.Add(unit);
+
+                Controls.Add(unit);
+
+            }
+        }
+
+        private void Update(object myObject, EventArgs e)
+        {
+            SnakeMovement();
+
+            EatFruit();
+
+            StopGameTrigger();
+        }
+
+        private void SnakeMovement()
+        {
+            for (int i = score; i >= 1; i--)
+            {
+                snake[i].Location = snake[i - 1].Location;
+            }
+
+            snake[0].Location = new Point(snake[0].Location.X + directionX * unitSize, snake[0].Location.Y + directionY * unitSize);
+        }
+
+        private void GetInput(object sender, KeyEventArgs e)
+        {
+            switch (e.KeyCode.ToString())
+            {
+                case "Right":
+                case "D":
+                    if (tempMove != "A")
+                    {
+                        directionX = 1;
+                        directionY = 0;
+
+                        tempMove = "D";
+                    }
+                    break;
+                case "Left":
+                case "A":
+                    if (tempMove != "D")
+                    {
+                        directionX = -1;
+                        directionY = 0;
+
+                        tempMove = "A";
+                    }
+                    break;
+                case "Up":
+                case "W":
+                    if (tempMove != "S")
+                    {
+                        directionX = 0;
+                        directionY = -1;
+
+                        tempMove = "W";
+                    }
+                    break;
+                case "Down":
+                case "S":
+                    if (tempMove != "W")
+                    {
+                        directionX = 0;
+                        directionY = 1;
+
+                        tempMove = "S";
+                    }
+                    break;
+            }
+        }
+
+        private void GameOver()
+        {
+            timer.Stop();
+
+            var result = MessageBox.Show($"YOU LOOOSE\nYOUR SCORE : {score}\nDO YOU WANNA TRY AGAIN???", "GAME OVER", MessageBoxButtons.OKCancel);
+
+            if (result == DialogResult.OK)
+            {
+                Application.Restart();
+            }
+            else
+            {
+                Application.Exit();
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+}
